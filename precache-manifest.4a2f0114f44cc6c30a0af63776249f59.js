246d3c62d4f11f3bb9d241a50a08add528c141ef
KaWaite <kyle.waite@outlook.com>
2020-07-30 11:16:43 +0900

    Updates

---
+++ precache-manifest.4a2f0114f44cc6c30a0af63776249f59.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f85177cc8665bf1d6c3f64cc2791c6c",
+    "url": "/star-struck/index.html"
+  },
+  {
+    "revision": "c20ded45959f08fe7168",
+    "url": "/star-struck/static/css/2.e884dcd1.chunk.css"
+  },
+  {
+    "revision": "59f2798c315af4ce6124",
+    "url": "/star-struck/static/css/main.3a446d74.chunk.css"
+  },
+  {
+    "revision": "c20ded45959f08fe7168",
+    "url": "/star-struck/static/js/2.8c2f10f1.chunk.js"
+  },
+  {
+    "revision": "981d302396af250969551bb21c06e41c",
+    "url": "/star-struck/static/js/2.8c2f10f1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59f2798c315af4ce6124",
+    "url": "/star-struck/static/js/main.fbf5662c.chunk.js"
+  },
+  {
+    "revision": "78f946b586168e3a9175",
+    "url": "/star-struck/static/js/runtime-main.1045edf3.js"
+  },
+  {
+    "revision": "733115f68985d137ad28a6efb4283575",
+    "url": "/star-struck/static/media/480px-Mars.733115f6.jpg"
+  },
+  {
+    "revision": "dbd7a6d43e819f893cfaca5d05fb89bf",
+    "url": "/star-struck/static/media/480px-Neptune.dbd7a6d4.jpg"
+  },
+  {
+    "revision": "5e69d0a1752eb13e96d63cdef794ee6d",
+    "url": "/star-struck/static/media/480px-Uranus.5e69d0a1.jpg"
+  },
+  {
+    "revision": "c16b545f54357d0b424ea17cba359ba0",
+    "url": "/star-struck/static/media/483px-Earth.c16b545f.png"
+  },
+  {
+    "revision": "d0960c8a4817cc66736d13bf98b2e21f",
+    "url": "/star-struck/static/media/484px-Mercury.d0960c8a.jpg"
+  },
+  {
+    "revision": "931bfb1ba9c8e8c4142dca13f29a9ce5",
+    "url": "/star-struck/static/media/640px-Saturn.931bfb1b.jpg"
+  },
+  {
+    "revision": "fe8b42d69938e9ebac958af80adfb86d",
+    "url": "/star-struck/static/media/Earth_large.fe8b42d6.png"
+  },
+  {
+    "revision": "4c5bf84cef66019a66e5d85db6208b8e",
+    "url": "/star-struck/static/media/Jupiter.4c5bf84c.jpg"
+  },
+  {
+    "revision": "188b5836f08b00ce96ff13fb1c0e721e",
+    "url": "/star-struck/static/media/Jupiter_large.188b5836.jpg"
+  },
+  {
+    "revision": "2261e17192cb3b88885a0a2d61b7e380",
+    "url": "/star-struck/static/media/Mars_large.2261e171.jpg"
+  },
+  {
+    "revision": "ffe05fa5aa01793e50c514f5354a554d",
+    "url": "/star-struck/static/media/Mercury_large.ffe05fa5.jpg"
+  },
+  {
+    "revision": "e152d084be8ec127cc37edccb998be84",
+    "url": "/star-struck/static/media/Neptune_large.e152d084.jpg"
+  },
+  {
+    "revision": "33bb685d3e7244f1554955cf0b06cc7e",
+    "url": "/star-struck/static/media/Saturn_large.33bb685d.jpg"
+  },
+  {
+    "revision": "35fbaafcb610ba4f5f789dfae65f0a7b",
+    "url": "/star-struck/static/media/Uranus_large.35fbaafc.jpg"
+  },
+  {
+    "revision": "636d1391140389964e0b6c50faea3ba1",
+    "url": "/star-struck/static/media/astro.636d1391.jpg"
+  },
+  {
+    "revision": "1cbc2be082a90974bf3fa0f3a3c2dbf1",
+    "url": "/star-struck/static/media/launch.1cbc2be0.jpg"
+  },
+  {
+    "revision": "57ad5dc1533ce70c490c8707662996c7",
+    "url": "/star-struck/static/media/moon.57ad5dc1.jpg"
+  },
+  {
+    "revision": "99af4ae883bef44b58a3e24b42c4af7e",
+    "url": "/star-struck/static/media/space.99af4ae8.jpg"
+  },
+  {
+    "revision": "76aa3beab369335c44d32dbf95dcb6e5",
+    "url": "/star-struck/static/media/venus.76aa3bea.jpg"
+  }
+]);
